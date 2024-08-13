95cf17ec2c5c3d7ecd0922488cb5c04fea3c9c16
abin c joseph <abincjoseph77@gmail.com>
2024-08-13 19:36:15 +0530

    registration_errors

--- cretal/cretal/accounts/views.py
+++ cretal/cretal/accounts/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import RegistrationForms
 from .models import Account
-# Create your views here.
+from django.views.decorators.cache import cache_control
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def Register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
     if request.method == 'POST':
         form = RegistrationForms(request.POST)
         if form.is_valid():
@@ -11,8 +15,8 @@ def Register(request):
             last_name = form.cleaned_data['last_name']
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
-            password  = form.cleaned_data['password']
-            username  = email.split('@')[0]
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
 
             user = Account.objects.create_user(
                 first_name=first_name,
@@ -21,14 +25,18 @@ def Register(request):
                 username=username,
                 password=password
             )
-            user.phone_number =  phone_number
+            user.phone_number = phone_number
             user.save()
+
+            return redirect('login')
+
     else:
         form = RegistrationForms()
+
     context = {
-        'form':form,
+        'form': form,
     }
-    return render(request,'accounts/register.html',context)
+    return render(request, 'accounts/register.html', context)
 
 
 def login(request):
