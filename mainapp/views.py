d59bd905b37e08b97b93191c164c87ca1bd52657
abin c joseph <abincjoseph77@gmail.com>
2025-02-20 13:00:56 +0530

    login problem completed

--- mainapp/views.py
+++ mainapp/views.py
@@ -48,17 +48,20 @@ def base(request):
         'products':products
     }
     return render(request,'mainapp/home.html',context)
+
 #locals() is a built in function to call all the local functions
 class Category(View):
     def get(self,request,val):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title')
         return render(request,'mainapp/category.html',locals())
+    
 class CategoryTitle(View):
     def get(self,request,val):
         product = Product.objects.filter(title=val)
         title = Product.objects.filter(category=product[0].category).values('title')
         return render(request,'mainapp/category.html',locals())
+    
 class ProductDetails(View):
     def get(self, request,pk):
         product = Product.objects.get(pk=pk)
