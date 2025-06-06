cd38a27014fe88498eb7206c455d7a3e0ecb67f1
abin c joseph <abincjoseph77@gmail.com>
2025-06-06 15:33:40 +0530

    Enable Cloudinary media storage for Render

--- cretal/settings.py
+++ cretal/settings.py
@@ -151,11 +151,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-#     'API_KEY': config('CLOUDINARY_API_KEY'),
-#     'API_SECRET': config('CLOUDINARY_API_SECRET')
-# }
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET')
+}
 
 # EMAIL_USE_TLS=True
 # EMAIL_HOST_USER='EMAIL_HOST_USER'
